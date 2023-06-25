4afc27dece96f6ce17a60de4777008ade00d82f4
ainmx <xxainmxx@protonmail.com>
2023-06-25 19:52:15 +0200

    Trying Mermaid diagrams

--- assets/selectAll-c295459b-36cb966b.js
+++
@@ -1,1 +0,0 @@
-import{bf as t,bg as n,bh as o}from"./index-681f1a2d.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
